8fb17306611a1b80c676e79aa54e6ca0207eaff9
vinilope <vinicius.zirondi@sptech.school>
2024-09-01 19:39:16 -0300

    add estrutura base

---
+++ src/test/java/com/serenity/api/serenity/SerenityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serenity.api.serenity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerenityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
