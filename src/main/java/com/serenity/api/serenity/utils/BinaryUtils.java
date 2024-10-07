6babdf32526eecf41e7b9a6ab0412972f7da8fb9
LukaTamazato <luka.tamazato@sptech.school>
2024-10-07 19:44:38 -0300

    feat - Lista generica e Pesquisa binaria @LukaTamazato

---
+++ src/main/java/com/serenity/api/serenity/utils/BinaryUtils.java
@@ -0,0 +1,25 @@
+package com.serenity.api.serenity.utils;
+
+public class BinaryUtils {
+        public static int binarySearch(int[] vetor,int indiceBuscado){
+            int contador = 0;
+            int inferior = 0,
+                    superior = vetor.length-1,
+                    meio;
+
+            for (int i = 0; i < vetor.length; i++) {
+                contador++;
+                meio = (inferior+superior)/2;
+                if (indiceBuscado == vetor[meio]){
+                    System.out.println(contador);
+                    return meio;
+                }else if(indiceBuscado > vetor[meio]){
+                    inferior = meio+1;
+                }else{
+                    superior = meio-1;
+                }
+            }
+            System.out.println(contador);
+            return -1;
+        }
+}
